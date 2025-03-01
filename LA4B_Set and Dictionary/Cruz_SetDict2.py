6d25d7ca53a72698afb07b90d0cc465fa2994a2c
Itzkoki <cruzkiko2822@gmail.com>
2025-03-01 10:06:11 +0800

    03/01/2025

--- LA4B_Set and Dictionary/Cruz_SetDict2.py
+++ LA4B_Set and Dictionary/Cruz_SetDict2.py
@@ -1,11 +1,15 @@
-#2.) create a program that will convert a certain input currency given the dollar to be converted.
+# 2. currency exchange
+exchangerates = {}
+with open("LA4B_Set and Dictionary/currency.csv") as file:
+    next(file) 
+    exchangerates = {line.split(",")[0]: (line.split(",")[1], float(line.split(",")[2])) for line in file}
 
+amount = float(input("How much dollar do you have? "))
+currency = input("What currency you want to have? ").upper()
 
-
-amount = input("How much dollar do you have: ") #100
-currency = input("What currency you want to have: ") #PHP
-
-result = amount % currency.csv
-
-print("Dollar:", amount + currency)
-print("Phillipines Peso:", amount + currency)
+if currency in exchangerates:
+    name, rate = exchangerates[currency]
+    print(f"\nDollar: {amount} USD")
+    print(f"{name}: {amount * rate}")
+else:
+    print("Currency not found.")
