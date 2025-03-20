70a502000c69ea3bc5dcf7b5e561b9d92066049e
Itzkoki <167260760+Itzkoki@users.noreply.github.com>
2025-03-20 21:15:48 +0800

    Update Cruz_OOP&Exception.py

--- Activity 06/Cruz_OOP&Exception.py
+++ Activity 06/Cruz_OOP&Exception.py
@@ -109,11 +109,4 @@ def main():
             print("Invalid choice. Please select a valid option.")
             
 if __name__ == "__main__":
-    main()
-    
-
-        
-        
-        
-        
-        
+    main()  
